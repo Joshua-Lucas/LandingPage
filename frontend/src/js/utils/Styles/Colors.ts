b8472722d2f34e48b3e730c7f68df375681365a1
Joshua Lucas <joshuamlucas91@gmail.com>
2020-08-29 17:31:02 -0400

    create theme and global styles

---
+++ frontend/src/js/utils/Styles/Colors.ts
@@ -0,0 +1,16 @@
+const Colors = {
+  MainBrand: '#4D23ff', // rgb(77,35,255)
+  Accent: {
+    Light: '#EEE', // rgb(238,238,238)
+    Dark: '#263063', // rgb(38,48,99)
+  },
+  Neutral: {
+    Light: '#FFF',
+    Dark: '#3C3C3C', // rgb(60,60,60)
+  },
+  Text: {
+    Main: '#252525', // rgb(37,37,37)
+    SignUp: '#444444', // rgb(68,68,68)
+  },
+}
+export default Colors
---
+++ frontend/src/js/utils/Styles/Theme.ts
@@ -0,0 +1,19 @@
+import Colors from './Colors'
+
+const Theme = {
+  // Brand Colors
+  mainBrand: Colors.MainBrand,
+  accentLight: Colors.Accent.Light,
+  accentDark: Colors.Accent.Dark,
+  neutralLight: Colors.Neutral.Light,
+  neutralDark: Colors.Neutral.Dark,
+
+  // Text Colors
+  mainText: Colors.Text.Main,
+  headerLight: Colors.Neutral.Light,
+  headerDark: Colors.Neutral.Dark,
+  signupTextLight: Colors.Accent.Light,
+  signupTextDark: Colors.Text.SignUp,
+}
+
+export default Theme
