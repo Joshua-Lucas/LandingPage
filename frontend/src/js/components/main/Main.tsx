8fd5b7dd16a2337e6defc3c2039575417ae55189
Joshua Lucas <joshuamlucas91@gmail.com>
2020-08-31 22:38:01 -0400

    add img sections and styeld

--- frontend/src/js/components/main/Main.tsx
+++ frontend/src/js/components/main/Main.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import styled from 'styled-components'
 import Banner from './banner/Banner'
+import Sections from './sections/Sections'
 
 //Styled Components
 const MainContent = styled.main``
 //Interfaces
-interface ImainProps {}
 
 //React Component
-const Main: React.FC<ImainProps> = ({}) => {
+const Main: React.FC = () => {
   return (
     <MainContent>
       <Banner />
+      <Sections />
     </MainContent>
   )
 }
